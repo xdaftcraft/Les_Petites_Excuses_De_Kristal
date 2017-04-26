f64fcfeaeced232c06bb8aac1f28c22b4946788a
xdaftcraft <anthony@chistel.fr>
2017-04-26 13:34:53 +0200

    Create wow.js

---
+++ js/wow.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+	function Get(url){
+		var Httpreq = new XMLHttpRequest();
+		Httpreq.open("GET",url,false);
+		Httpreq.send(null);
+		return Httpreq.responseText;
+	}
+
+	var apikey = "dphv5ptd7qhzpmhercyje6jamza64xe6";
+	var server = "chogall";
+	var guild = "Discorde";
+	var guild = JSON.parse(Get("https://eu.api.battle.net/wow/guild/" + server +"/" + guild + "?fields=members&locale=fr_FR&apikey=" + apikey ));
+
+	console.log(guild);
+
+	$("#content").append(guild.name + " ("+guild.realm+")"+"<br><br>");
+	for(var i=1; i < guild.members.length;i++) {
+		if(guild.members[i].character.level === 110 && guild.members[i].rank <=5) {
+			$("#content").append(guild.members[i].character.name + "<br>");
+		}
+	}
+	
+});
