b5743227be717ca8085958204c2b6c687d9823f7
xdaftcraft <anthony@chistel.fr>
2017-05-09 14:36:35 +0200

    Update wow.js

--- js/wow.js
+++ js/wow.js
@@ -7,20 +7,23 @@ $(document).ready(function() {
 		return Httpreq.responseText;
 	}
 
-	var apikey = "dphv5ptd7qhzpmhercyje6jamza64xe6";
-	var server = "chogall";
-	var guild = "Discorde";
-	var guild = JSON.parse(Get("https://eu.api.battle.net/wow/guild/" + server +"/" + guild + "?fields=members&locale=fr_FR&apikey=" + apikey ));
+	//var apikey = "dphv5ptd7qhzpmhercyje6jamza64xe6";
+	//var server = "chogall";
+	//var guild = "Discorde";
+	//var guild = JSON.parse(Get("https://eu.api.battle.net/wow/guild/" + server +"/" + guild + "?fields=members&locale=fr_FR&apikey=" + apikey ));
 
 	console.log(guild);
+	var guild = JSON.parse(Get("https://www.wowprogress.com/guild/eu/cho-gall/Discorde/json_rank"));
 
-	$("#content").append(guild.name + " ("+guild.realm+")"+"<br><br>");
-	for(var i=1; i < guild.members.length;i++) {
-		if(guild.members[i].character.level === 110 && guild.members[i].rank <=5) {
-			$("#content").append(guild.members[i].character.name + "<br>");
-			if (guildDat.members[i].character.class === 8)
+	console.log(guild);
+	$("#content").append(guild.realm_rank);
+	//$("#content").append(guild.name + " ("+guild.realm+")"+"<br><br>");
+	//for(var i=1; i < guild.members.length;i++) {
+	//	if(guild.members[i].character.level === 110 && guild.members[i].rank <=5) {
+	//		$("#content").append(guild.members[i].character.name + "<br>");
+	//		if (guildDat.members[i].character.class === 8)
 				
-		}
-	}
+	//	}
+	//}
 	
 });
